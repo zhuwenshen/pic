da5828c795e0e17e4ca425581e94eb2e48bb5e49
zhuwenshen <1204621904@qq.com>
2018-03-20 15:23:24 +0800

    初始化

---
+++ pic/src/main/java/com/zhuwenshen/PicApplication.java
@@ -0,0 +1,12 @@
+package com.zhuwenshen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicApplication.class, args);
+	}
+}
